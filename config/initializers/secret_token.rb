f45b0d8b81f88960bd9a0bca0c7d2c614320c1b6
Воробьев <voronsupermen@gmail.com>
2013-01-27 17:02:15 +0300

    Инициализация приложения вместе с добавлением вендорных библиотек, от которых зависит моя библиотека. Основная сущность- user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneLib::Application.config.secret_token = '74f7333e7bdab094b727fb361bcfcb514dd63506562ac8eb1699e2bbd148ece2da41028411ce6ca28b749b5f2ae3bd2298458a3e683bc1d82697fd26305f6615'
