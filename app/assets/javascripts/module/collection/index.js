09e5d8c86f688b170c47019b46be6616d8f9f5db
vora-bei <voronsupermen@gmail.com>
2013-01-29 17:33:22 +0300

    Коллекции и Модели добавил

---
+++ app/assets/javascripts/module/collection/index.js
@@ -0,0 +1,43 @@
+TiragSales.Collections.Basic = Backbone.Collection.extend({
+    filter: {},
+    category : '',
+    initialize: function(models,options){
+        this.filter= {};
+        if((options!=undefined)&&(options.url!=undefined))
+            this.url_base=options.url;
+        if((options!=undefined)&&(options.filter!=undefined))
+            this.filter['filter']=options.filter;
+        var self =this;
+        if(_.isEmpty(models))
+            this.fetch();
+        else
+            setTimeout(function(){self.trigger('reset')},100)
+
+    },
+    url_base : "",
+    model: TiragSales.Models.contract,
+    url_delete: function(id){
+        return  this.url_base+'/'+id;
+    },
+    url: function(){
+        var url;
+
+        if(!_.isEmpty(this.filter))
+        {
+            url =  _.reduce(this.filter,function(memo,value,key,list){
+                if(value.indexOf('=')+1)
+                    return memo+value+'&'
+                return memo+key+'='+value+'&'
+            },this.url_base+'?');
+        }
+        else
+        {
+            url=this.url_base+'?';
+        }
+        url+=this.category;
+
+        return url;
+    }
+
+
+});
---
+++ app/assets/javascripts/module/model/index.js
@@ -0,0 +1,20 @@
+TiragSales.Models.Basic = Backbone.Model.extend({
+    nameModel : 'model',
+    get_relational: function (name){
+        if(_.isEmpty(this.collection[name]))
+            this.collection[name]={};
+        var id=this.id;
+        if(_.isEmpty(this.collection[name][id])){
+            this.collection[name][id]=new this['_'+name](this.get(name),{belongs_to: this})
+            delete this.attributes[name]
+        }
+        else{
+            if(this.get(name)){
+                this.collection[name][id]['belongs_to']=this;
+                this.collection[name][id].reset(this.get(name))
+                delete this.attributes[name]
+            }
+        }
+        return this.collection[name][id];
+    }
+});
