c472dc2e25188a8fca55a09506242e98ea161041
Vorobyov <voronsupermen@gmail.com>
2013-02-26 14:34:56 +0400

    LOl

--- app/assets/javascripts/Backbone/model/User.js
+++ app/assets/javascripts/Backbone/model/User.js
@@ -1,18 +1,18 @@
 TiragSales.Models.User = BackList.Models.Basic.extend({
-    nameModel:'user'
-    /*
-    url_for_delete : function(){
-     при определении этой функции переопределяется урл для удаления элемента
-    },
-    */
-    /*
+    nameModel:'user',
+
     url_for_update_name: function(){
-      при определении генерирует урл для апдейта конкретного параметра модели
-      этот например для апдейта параметра 'name'
-     url_for_update_email() для апдейта параметра email
+        return 'users_name/1'
     },
-    */
-<<<<<<< HEAD
+    url_for_update_title: function(){
+        return 'users/1'
+    },
+    url:function() {
+        var base = _.result(this, 'urlRoot') || _.result(this.collection, 'url') || urlError();
+        if (this.isNew()) return base;
+        base=base.split('?')[0];
+        return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id);
+    }
 })
 
 
@@ -29,18 +29,6 @@ TiragSales.Models.UserWithRelational = BackList.Models.Basic.extend({
 })
 
 
-=======
-    url_for_update_name: function(){
-        return 'users_name/1'
-    },
-    url_for_update_title: function(){
-        return 'users/1'
-    },
-    url:function() {
-        var base = _.result(this, 'urlRoot') || _.result(this.collection, 'url') || urlError();
-        if (this.isNew()) return base;
-        base=base.split('?')[0];
-        return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id);
-    }
-})
->>>>>>> 7067a68691dd30391c2101a0572a862b2f959959
+
+
+
